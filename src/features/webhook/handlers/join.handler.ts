2bcb21ba4b5649b244092cf67576830ec496c9aa
Kohei_Seki <sekikohei1010@gmail.com>
2026-05-03 00:57:39 +0900

    implement handler

---
+++ src/features/webhook/handlers/join.handler.ts
@@ -0,0 +1,16 @@
+import { lineClient } from "@/lib/line/client";
+import { messagingApi } from "@line/bot-sdk";
+
+export async function handleJoin(replyToken: string, groupId: string) {
+	const liffUrl = `https://liff.line.me/${process.env.NEXT_PUBLIC_LIFF_ID}?groupId=${groupId}`;
+
+	await lineClient.replyMessage({
+		replyToken,
+		messages: [
+			{
+				type: "text",
+				text: `参加登録\n${liffUrl}`,
+			} satisfies messagingApi.TextMessage,
+		],
+	});
+}
