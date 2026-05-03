84bf3482b65db60d44bdecff346dc317fe2c2f52
Kohei_Seki <sekikohei1010@gmail.com>
2026-05-04 01:20:41 +0900

    implement bussiness logic

--- src/features/expenses/types/expense.types.ts
+++ src/features/expenses/types/expense.types.ts
@@ -45,21 +45,3 @@ export type ExpenseForm = {
 	/** 支出参加者 */
 	expenseParticipants: ExpenseParticipant[];
 };
-
-/** ユーザーの残高 */
-export type UserBalance = {
-	/** ラインユーザーID */
-	lineUserId: string;
-	/** 残高 */
-	netBalance: number;
-};
-
-/** 精算結果 */
-export type Settlement = {
-	/** 送金者ユーザーID */
-	fromUserId: string;
-	/** 受取者ユーザーID */
-	toUserId: string;
-	/** 支払い額 */
-	amount: number;
-};
