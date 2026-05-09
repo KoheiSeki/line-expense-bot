c5283ff8b7adfbfb4371069e2bc769bb6be07749
Kohei_Seki <sekikohei1010@gmail.com>
2026-05-09 18:24:23 +0900

    implement line-message error handler

--- src/features/webhook/handlers/message.handler.ts
+++ src/features/webhook/handlers/message.handler.ts
@@ -1,10 +1,11 @@
-import { webhook } from "@line/bot-sdk";
+import { messagingApi, webhook } from "@line/bot-sdk";
 import { handleRegister } from "./register.handler";
 import { handleJoin } from "./join.handler";
 import { handleDisplay } from "./display.handler";
 import { handleHelp } from "./help.handler";
 import { handleClose } from "./close.handler";
 import { handleReopen } from "./reopen.handler";
+import { lineClient } from "@/lib/line/client";
 
 /**
  * メッセージイベントを処理するハンドラー
@@ -20,25 +21,39 @@ export async function handleMessage(event: webhook.MessageEvent) {
 	const groupId = event.source.groupId;
 	const replyToken = event.replyToken!;
 
-	switch (text) {
-		case "参加":
-			await handleJoin(replyToken, groupId);
-			break;
-		case "登録":
-			await handleRegister(replyToken, groupId);
-			break;
-		case "表示":
-			await handleDisplay(replyToken, groupId);
-			break;
-		case "完了":
-			await handleClose(replyToken, groupId);
-			break;
-		case "締め解除":
-			await handleReopen(replyToken, groupId);
-			break;
-		case "ヘルプ":
-			await handleHelp(replyToken);
-			break;
-		default:
+	try {
+		switch (text) {
+			case "参加":
+				await handleJoin(replyToken, groupId);
+				break;
+			case "登録":
+				await handleRegister(replyToken, groupId);
+				break;
+			case "表示":
+				await handleDisplay(replyToken, groupId);
+				break;
+			case "完了":
+				await handleClose(replyToken, groupId);
+				break;
+			case "締め解除":
+				await handleReopen(replyToken, groupId);
+				break;
+			case "ヘルプ":
+				await handleHelp(replyToken);
+				break;
+			default:
+		}
+	} catch (err) {
+		console.error("[Error] Failed to handle message", err);
+
+		await lineClient.replyMessage({
+			replyToken,
+			messages: [
+				{
+					type: "text",
+					text: "エラーが発生しました。時間をおいて再度お試しください。",
+				} satisfies messagingApi.TextMessage,
+			],
+		});
 	}
 }
