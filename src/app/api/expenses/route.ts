a6b7368d91e14295cc1e7006ba6ce4e0e83d30a0
Kohei_Seki <sekikohei1010@gmail.com>
2026-05-10 23:54:07 +0900

    implement edit expense api end-point

--- src/app/api/expenses/route.ts
+++ src/app/api/expenses/route.ts
@@ -1,4 +1,6 @@
+import { editExpense } from "@/features/expenses/service/edit-expense.server.service";
 import { createExpense } from "@/features/expenses/service/expense.server.service";
+import { EditExpenseReq } from "@/features/expenses/types/edit-expense.types";
 import { CreateExpenseReq } from "@/features/expenses/types/expense.types";
 import { apiHandler } from "@/lib/api/api-handler";
 import { NextRequest, NextResponse } from "next/server";
@@ -16,6 +18,24 @@ export const POST = apiHandler(
 
 		return NextResponse.json(
 			{ message: "支出を登録しました" },
+			{ status: 200 },
+		);
+	},
+);
+
+/**
+ * 支出を編集するAPI
+ * @param req リクエスト
+ * @returns レスポンス
+ */
+export const PUT = apiHandler(
+	async (req: NextRequest): Promise<NextResponse> => {
+		const body: EditExpenseReq = await req.json();
+
+		await editExpense(body);
+
+		return NextResponse.json(
+			{ message: "支出を編集しました" },
 			{ status: 200 },
 		);
 	},
